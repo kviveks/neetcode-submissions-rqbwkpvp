4a29688f574711d3c8913b864b5b743bd45abbe3
kviveks <kviveksinghs@gmail.com>
2026-04-12 01:35:47 +0530

    Add: ipo - submission-0

---
+++ Data Structures & Algorithms/ipo/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length;
+        int[][] projects = new int[n][2];
+
+        for(int i=0;i<n;i++){
+            projects[i][0] = capital[i];
+            projects[i][1] = profits[i];
+        }
+
+
+        Arrays.sort(projects,(a,b)->(a[0]-b[0]));
+
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->(b-a));
+        int i = 0;
+
+        while(k-->0){
+
+            while(i<n && w>= projects[i][0]){
+                pq.add(projects[i][1]);
+                i++;
+            }
+
+            if(pq.isEmpty())
+                break;
+
+            w+=pq.remove();
+        }
+
+        return w;
+    }
+}
