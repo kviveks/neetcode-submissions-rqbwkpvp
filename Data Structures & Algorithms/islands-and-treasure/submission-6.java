a8adcbdd2e33ef71e8e602b2866105c2e354a666
kviveks <kviveksinghs@gmail.com>
2026-04-23 14:33:12 +0530

    Add: islands-and-treasure - submission-6

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-6.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void islandsAndTreasure(int[][] grid) {
+        Deque<int[]> queue = new ArrayDeque<>();
+
+
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==0){
+                    queue.addLast(new int[]{i,j});
+                }
+            }
+        }
+
+        int[][] dirs = {{-1,0},{1,0},{0,1},{0,-1}};
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+
+            for(int i=1;i<=size;i++){
+                int[] coord = queue.removeFirst();
+                int x = coord[0];
+                int y = coord[1];
+
+                for(int[] dir : dirs){
+                    int x_ = x+dir[0];
+                    int y_ = y+dir[1];
+
+                    if(x_<0||x_>=grid.length||y_<0||y_>=grid[0].length){
+                        continue;
+                    }
+
+                    if(grid[x_][y_]!=Integer.MAX_VALUE){
+                        continue;
+                    }
+
+                    grid[x_][y_]=grid[x][y]+1;
+                    queue.addLast(new int[]{x_,y_});
+                }
+            }
+        }
+
+    }
+}
