d28952632ad3bccfcbbbbbdd536727b98b7510f5
kviveks <kviveksinghs@gmail.com>
2026-05-01 13:55:09 +0530

    Add: house-robber-ii - submission-5

---
+++ Data Structures & Algorithms/house-robber-ii/submission-5.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if(n==0){
+            return 0;
+        }else if(n==1){
+            return nums[0];
+        }
+
+        int[] dp = new int[n];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0],nums[1]);
+
+
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
+        }
+
+        int maxAmount = dp[n-2];
+
+        Arrays.fill(dp,0);
+
+        dp[0]=0;
+        dp[1]=nums[1];
+
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
+        }
+
+
+        return Math.max(maxAmount,dp[n-1]);
+
+    }
+}
