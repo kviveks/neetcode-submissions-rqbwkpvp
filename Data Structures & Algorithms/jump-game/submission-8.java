9b68a5f9dd92e41632c513537b328d8219353e6d
kviveks <kviveksinghs@gmail.com>
2026-05-05 00:32:38 +0530

    Add: jump-game - submission-8

---
+++ Data Structures & Algorithms/jump-game/submission-8.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxJump = 0;
+
+        for(int i=0;i<nums.length;i++){
+            if(maxJump<i){
+                break;
+            }
+
+            maxJump = Math.max(maxJump,i+nums[i]);
+            if(maxJump>=nums.length-1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
