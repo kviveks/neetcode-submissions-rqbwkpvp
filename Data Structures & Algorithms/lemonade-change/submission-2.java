047907d463ae6554d4111c8a0a6983d5233dd4d9
kviveks <kviveksinghs@gmail.com>
2026-06-13 18:10:43 +0530

    Add: lemonade-change - submission-2

---
+++ Data Structures & Algorithms/lemonade-change/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+
+        for(int bill : bills){
+            if(bill == 5){
+                five++;
+            }else if(bill==10){
+                five--;
+                ten++;
+            }else if(ten > 0){
+                five--;
+                ten--;
+            }else{
+                five=five-3;
+            }
+
+            if(five<0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
