2a67c0ec97ec694d301b53d564e662c157d420e6
kviveks <kviveksinghs@gmail.com>
2026-06-13 18:25:12 +0530

    Add: maximum-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-subarray/submission-4.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentMax = nums[0];
+        int globalMax = nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            currentMax = Math.max(currentMax+nums[i],nums[i]);
+            globalMax = Math.max(currentMax,globalMax); 
+        }
+
+        return globalMax;
+    }
+}
